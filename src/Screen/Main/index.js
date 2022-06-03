d18ebd2b72325051b2059ca813acf0e7145f881b
Shahbaz Ali <75558726+mshahbazali@users.noreply.github.com>
2022-06-03 07:50:44 -0700

    Second

--- src/Screen/Main/index.js
+++ src/Screen/Main/index.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, RefreshControl, ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import * as Network from 'expo-network';
 import NetInfo from "@react-native-community/netinfo";
@@ -7,7 +7,9 @@ import * as Location from 'expo-location';
 import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import * as Application from 'expo-application';
-
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 export default function index() {
     const [macAddress, setMacAddress] = useState()
     const [vendor, setVendor] = useState()
@@ -19,65 +21,121 @@ export default function index() {
     const [bssid, setBssid] = useState()
     const [os, setOs] = useState()
     const [osVersion, setOsVersion] = useState()
-
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
     useEffect(() => {
-        setVendor(Device.manufacturer)
-        setBrand(Device.brand)
-        setModel(Device.modelName)
+        setVendor(Device.manufacturer);
+        setBrand(Device.brand);
+        setModel(Device.modelName);
         NetInfo.addEventListener(state => {
-            state.isConnected == true ? setWifi("Enable") : setWifi("Disable")
-            setBssid(state.details.bssid)
-            setSsid(state.details.ssid)
+            state.isConnected == true ? setWifi("Enable") : setWifi("Disable");
+            setBssid(state.details.bssid);
+            setSsid(state.details.ssid);
         });
         Network.getIpAddressAsync().then((ip) => {
             setIp(ip)
-        })
-        setOs(Device.osName)
-        setOsVersion(Device.osVersion)
-        Location.requestForegroundPermissionsAsync().then((permission) => { })
+        });
+        setOs(Device.osName);
+        setOsVersion(Device.osVersion);
+        Location.requestForegroundPermissionsAsync().then((permission) => { });
 
     }, [])
-    const a = ''
     useEffect(() => {
-        if (os === "Android") {
-            setMacAddress(Application.androidId)
-            console.log(Application.androidId);
-            console.log(os);
+        const macAddress = async () => {
+            if (Device.osName == "Android") {
+                setMacAddress(Application.androidId)
+            }
+            else {
+                await Application.getIosIdForVendorAsync().then((res) => {
+                    setMacAddress(res)
+                })
+            }
         }
-        else {
-            Application.getIosIdForVendorAsync().then((res) => {
-                setMacAddress(res)
-            })
-        }
+        macAddress()
     }, [macAddress])
-
+    const data = [
+        {
+            name: "Vendor",
+            value: vendor
+        },
+        {
+            name: "Brand",
+            value: brand
+        },
+        {
+            name: "Model",
+            value: model
+        },
+        {
+            name: "Wifi",
+            value: wifi
+        },
+        {
+            name: "Local Ip",
+            value: ip
+        },
+        {
+            name: "SSID",
+            value: ssid
+        },
+        {
+            name: "BSSID",
+            value: macAddress
+        },
+        {
+            name: "Operating System",
+            value: os
+        },
+        {
+            name: "OS Version",
+            value: osVersion
+        }
+    ]
     return (
         <View style={{ flex: 1 }}>
             <StatusBar style="auto" />
-            <View style={styles.header}>
-                <View>
-                    <Text style={styles.headerTitle}>Mac Address</Text>
-                </View>
-                <View>
+            <ScrollView
+                contentContainerStyle={{ flex: 1 }}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />}>
+                <View style={styles.header}>
                     <View>
-                        <Ionicons name="share-social" size={24} color="white" />
+                        <Text style={styles.headerTitle}>Mac Retriever</Text>
+                    </View>
+                    <View>
+                        <View>
+                            <Ionicons name="share-social" size={24} color="white" />
+                        </View>
                     </View>
                 </View>
-            </View>
-            <View>
-                <View style={styles.macAddressBox}>
-                    <Text style={styles.macText}>{macAddress}</Text>
-                </View>
-            </View>
-            <View style={{ marginHorizontal: 27, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                 <View>
-
+                    <View style={styles.macAddressBox}>
+                        <Text style={styles.macText}>{bssid}</Text>
+                    </View>
                 </View>
-                <View>
-
+                <View style={{ marginHorizontal: 27, alignItems: 'center', marginVertical: 20 }}>
+                    {
+                        data.map((e, i) => {
+                            return (
+                                <View key={i} style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+                                    <View style={{ borderBottomColor: "grey", borderBottomWidth: 1, paddingVertical: 5, width: '50%', marginVertical: 5 }}>
+                                        <Text style={{ fontSize: 16 }}>{e.name}</Text>
+                                    </View>
+                                    <View style={{ borderBottomColor: "grey", borderBottomWidth: 1, paddingVertical: 5, width: '50%', marginVertical: 5 }}>
+                                        <Text style={{ fontSize: 16 }}>{e.value}</Text>
+                                    </View>
+                                </View>
+                            )
+                        })
+                    }
                 </View>
-
-            </View>
+            </ScrollView>
         </View>
     )
 }
